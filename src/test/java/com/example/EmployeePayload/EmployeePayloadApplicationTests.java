18a3317e651b2a010225a556b22da53e95d7895d
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-05 17:11:42 +0530

    [Om Datt Tripathi] init. initial commit

---
+++ src/test/java/com/example/EmployeePayload/EmployeePayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePayload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
